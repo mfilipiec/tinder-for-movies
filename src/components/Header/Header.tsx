515667abee493f3e3efd3d778a6598dbf0ec5cb3
mfilipiec <michalfilipiec@gmail.com>
2025-01-03 01:19:12 +0100

    Fixed Logo src path

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import './Header.scss';
+import logo from '/favicon.ico';
 
 export default function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src="/favicon.ico" alt="TfM Logo" />
+      <img className="header__logo" src={logo} alt="TfM Logo" />
       <div className="header__content">
         <h1 className="header__title">Tinder for Movies</h1>
         <p className="header__subtitle">
