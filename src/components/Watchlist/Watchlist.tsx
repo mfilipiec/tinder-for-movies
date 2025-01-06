075b8839a6e14b9414b8ecaee75b681678953530
mfilipiec <michalfilipiec@gmail.com>
2025-01-07 00:30:07 +0100

    Added animation to Watchlist component ;)

--- src/components/Watchlist/Watchlist.tsx
+++ src/components/Watchlist/Watchlist.tsx
@@ -1,16 +1,30 @@
 import './Watchlist.scss';
 import StarIcon from '../../icons/StarIcon';
 import { Movie } from '../Discover/Discover';
+import { motion } from 'motion/react';
 
 export default function MovieListItem({ list }: { list: Movie[] }) {
   return (
     <div className="watchlist">
-      <h2 className="watchlist__header">Your watchlist:</h2>
-      {list.map((movie) => {
+      <motion.h2
+        className="watchlist__header"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        Your watchlist:
+      </motion.h2>
+      {list.map((movie, index) => {
         const { id, imageURL, title, summary, rating } = movie;
 
         return (
-          <div className="watchlist__item" key={id}>
+          <motion.div
+            className="watchlist__item"
+            key={id}
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.75 + index * 0.25, duration: 0.5 }}
+          >
             <img className="watchlist__image" src={imageURL} alt={title} />
             <div className="watchlist__details">
               <h3 className="watchlist__title">{title}</h3>
@@ -19,7 +33,7 @@ export default function MovieListItem({ list }: { list: Movie[] }) {
               </div>
               <p className="watchlist__summary">{summary}</p>
             </div>
-          </div>
+          </motion.div>
         );
       })}
     </div>
